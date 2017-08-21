d1f6c4215681874610f08727204afcbd861161f7
Fernando H. Cabral <hirocabral@hotmail.com>
2017-08-20 21:38:59 -0300

    Primeiro Commit

---
+++ 4 Semestre/Topicos Especiais/Aula 01/Ex1.js
@@ -0,0 +1,4 @@
+var qtd = parseInt(prompt('Digite a quantidade:'));
+var preco = parseFloat(prompt('Digite o valor unitário:'));
+var valor = qtd * preco;
+alert('O valor total é: '+valor);
---
+++ 4 Semestre/Topicos Especiais/Aula 02/Ex1.js
@@ -0,0 +1,5 @@
+var vetor = new array(7);
+for(i = 0; i < 7; i++){
+    vetor = prompt('Digite '+ (i+1)+' valores:');
+}
+
